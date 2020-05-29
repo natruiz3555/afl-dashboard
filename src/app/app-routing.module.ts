fb72795e0d6075ab09efc94c614c0d57874786d1
Jess Armstrong <jessarmstrong@Jesss-MacBook-Pro-3.local>
2020-05-29 12:16:37 +1000

    issue/6: Second widget done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ParentHeadComponent } from './parent-head/parent-head.component';
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,24 @@ import { GameServiceService } from './game-service.service';
 import { TipService } from './tip.service';
 import { PastGamesComponent } from './past-games/past-games.component';
 import { NextGamePredictionComponent } from './next-game-prediction/next-game-prediction.component';
+import { ParentHeadComponent } from './parent-head/parent-head.component';
+import { ChildHeadComponent } from './child-head/child-head.component';
+import { HeadServiceService } from './head-service.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     PastGamesComponent,
     NextGamePredictionComponent
+    ParentHeadComponent,
+    ChildHeadComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [GameServiceService, TipService, HttpClient],
+  providers: [GameServiceService, TipService, HttpClient, HeadServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
